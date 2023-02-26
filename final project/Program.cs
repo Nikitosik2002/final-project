e42caf9cecfdc21740330078ba4dabf3cd2ef6ee
Nikita <nikita.kopeickin@yandex.ru>
2023-02-26 18:58:12 +0300

    compleated

--- final project/Program.cs
+++ final project/Program.cs
@@ -3,7 +3,7 @@ int r = Convert.ToInt32(Console.ReadLine());
 int[] array = new int[r];
 string[] arrString = new string[r];
 
-void feelPrintArr (string[] ar)
+void feelArr (string[] ar)
 {
     for (int i = 0; i < ar.Length; i++)
     {
@@ -11,13 +11,43 @@ void feelPrintArr (string[] ar)
         ar[i] = Console.ReadLine();
     }
 
+}
+
+void printArr(string[] ar)
+{
     System.Console.WriteLine();
-for (int i = 0; i < arrString.Length; i++)
+    for (int i = 0; i < ar.Length; i++)
     {
-        System.Console.Write(arrString[i] + " ");
+        System.Console.Write(ar[i] + " ");
     }
 }
 
-feelPrintArr(arrString);
+feelArr(arrString);
+printArr(arrString);
 
 
+
+int count = 0;
+for (int i = 0; i < arrString.Length; i++)
+{
+    if(arrString[i].Length <= 3)
+    {
+        count += 1;
+    }
+   
+}
+string[] arrString2 = new string[count];
+
+int count2 = 0;
+string box = string.Empty;
+for (int i = 0; i < arrString.Length; i++)
+{
+    if(arrString[i].Length <= 3)
+    {
+        box = arrString[i];
+        arrString2[count2] = box;
+        count2 += 1;
+    }
+}
+
+printArr(arrString2);
