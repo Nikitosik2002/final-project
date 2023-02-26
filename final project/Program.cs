3562539a51770d1b9e6ff5d598f1ae4aacf849d8
Nikita <nikita.kopeickin@yandex.ru>
2023-02-26 17:55:36 +0300

    add feel and print arr

---
+++ final project/Program.cs
@@ -0,0 +1,23 @@
+﻿System.Console.WriteLine("Введите длинну массива");
+int r = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[r];
+string[] arrString = new string[r];
+
+void feelPrintArr (string[] ar)
+{
+    for (int i = 0; i < ar.Length; i++)
+    {
+        System.Console.WriteLine($"Введите {i} эллемент");
+        ar[i] = Console.ReadLine();
+    }
+
+    System.Console.WriteLine();
+for (int i = 0; i < arrString.Length; i++)
+    {
+        System.Console.Write(arrString[i] + " ");
+    }
+}
+
+feelPrintArr(arrString);
+
+
